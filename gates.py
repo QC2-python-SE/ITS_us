9121e4851976a4cdb2faf4940ecee011dfd1e558
Theo-Iosif <theodor.iosif1@gmail.com>
2024-11-01 12:51:36 +0000

    Added some relevant 2-qubit gates.

--- gates.py
+++ gates.py
@@ -5,10 +5,9 @@ class Gate(object):
     The Gate object is an n-qubit gate represented in the standard basis.
     
     Attributes:
-        num_qubits (int): The number of qubits that trhough the gate,
+        num_qubits (int): The number of qubits that pass through the gate,
         array (ndarray): 2^n by 2^n array representing the gate.
     """
-
 
     def __init__(self, num_qubits, array):
         """
@@ -34,28 +33,6 @@ class Gate(object):
         return copy
 
 
-def tensorprod(gate_list):
-    """
-    Calculates the tensor product of gates from a list.
-
-    Args:
-        gate_list (list): list of Gate objects
-    
-    Returns:
-        Gate object: resulting gate
-    """
-
-    tensor_array = gate_list[0].array
-    tensor_num_qubits = gate_list[0].num_qubits
-    
-    for gate in gate_list[1:]:
-        tensor_array = np.kron(tensor_array, gate.array)
-        tensor_num_qubits += gate.num_qubits
-    
-    tensor_gate = Gate(tensor_num_qubits, tensor_array)
-    return tensor_gate
-
-
 def create_gate(num_qubits, array):
     """
     Creates a customised gate U. It needs to be unitary or scalable to unitary, in which case it is automatically scaled to a unitary matrix.
@@ -65,7 +42,7 @@ def create_gate(num_qubits, array):
         array (array_like): A 2^n by 2^n array of floats, where n = num_qubits (int) is the number of qubits.
     
     Returns:
-        Gate object: the custom gate.
+        Gate: the custom gate.
 
     """
     
@@ -95,31 +72,94 @@ def create_gate(num_qubits, array):
     return gate
 
 
-def phi_gate(phi):
+def phase_gate(phase):
     """
-    Creates a phase gate with an input phase.
+    Creates a phase gate from a given input phase.
 
     Args:
-        phi (float): The input phase, must be a real number.
+        phase (float): The input phase, must be a real number.
     
     Returns:
-        Gate object: the corresponding phi gate.
+        Gate: the corresponding phase gate.
     """
-    if np.imag(phi) != 0:
+    if np.imag(phase) != 0:
         raise ValueError('Input phase is not real. The gate is not unitary.')
     
-    return Gate(1, [[1,0],[0,np.exp(1j*phi)]])
+    return Gate(1, [[1,0],[0,np.exp(1j*phase)]])
 
+
+def tensorprod(gate_list):
+    """
+    Calculates the tensor product of gates from a list.
+
+    Args:
+        gate_list (list): list of Gate objects
+    
+    Returns:
+        Gate: resulting gate
+    """
+
+    tensor_array = gate_list[0].array
+    tensor_num_qubits = gate_list[0].num_qubits
+    
+    for gate in gate_list[1:]:
+        tensor_array = np.kron(tensor_array, gate.array)
+        tensor_num_qubits += gate.num_qubits
+    
+    tensor_gate = Gate(tensor_num_qubits, tensor_array)
+    return tensor_gate
+
+
+def controlled_gate2(gate, control):
+    """
+    Controlled gate (custom) for 2 qubit systems, with the control from specified qubit.
+
+    Args:
+        gate (Gate): must be a 1-qubit gate.
+        control (int): if 1, the control is the first qubit. If 2, the control is the second qubit.
+    
+    Returns:
+        Gate: the controlled gate.
+    """
+    U_gate = gate.array
+    if control == 1:
+        return  Gate(2, [[1,0,0,0],[0,1,0,0],[0,0,U_gate[0,0],U_gate[0,1]],[0,0,U_gate[1,0],U_gate[1,1]]])
+    elif control == 2:
+        return  Gate(2, [[1,0,0,0],[0,U_gate[0,0],0,U_gate[0,1]],[0,0,1,0],[0,U_gate[1,0],0,U_gate[1,1]]])
+    else:
+        raise ValueError('The control must be 1 or 2 for a 2-qubit system.')
+
+
+def CNOT_gate2(control):
+    """
+    CNOT gate for 2 qubit systems, with the control from specified qubit.
+    This is equivalent to "controlled_gate2(X_gate, control)".
+
+    Args:
+        control (int): if 1, the control is the first qubit. If 2, the control is the second qubit.
+    
+    Returns:
+        Gate object: the CNOT gate.
+    """
+    if control == 1:
+        return  Gate(2, [[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+    elif control == 2:
+        return  Gate(2, [[1,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]])
+    else:
+        raise ValueError('The control must be 1 or 2 for a 2-qubit system.')
+    
 
 ### how do I docstring constants?
 
 X_gate = Gate(1, [[0,1],[1,0]])
 Y_gate = Gate(1, [[0,-1j],[1j,0]])
 Z_gate = Gate(1, [[1,0],[0,-1]])
-H_gate = Gate(1, [[1,1],[1,-1]]/np.sqrt(2))
+H_gate = Gate(1, np.array([[1,1],[1,-1]])/np.sqrt(2))
 S_gate = Gate(1, [[1,0],[0,1j]])
 T_gate = Gate(1, [[1,0],[0,(1+1j)/np.sqrt(2)]])
-CNOT_gate = Gate(2, [[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+SWAP_gate2 = Gate(2, [[1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])
+HH_gate = Gate(2, 0.5 * np.array([[1,1,1,1],[1,-1,1,-1],[1,1,-1,-1],[1,-1,-1,1]]))
+FT_gate = Gate(2, 0.5 * np.array([[1,1,1,1],[1,1j,-1,-1j],[1,-1,1,-1],[1,-1j,-1,1j]]))
 
 
 
