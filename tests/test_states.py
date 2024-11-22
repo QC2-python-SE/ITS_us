15217bbd5c4d65296eaace628d77b1cf2871bb41
sc-coding <scender508@gmail.com>
2024-11-22 20:40:57 +0000

    added imported libraries to test_states

--- tests/test_states.py
+++ tests/test_states.py
@@ -1,1 +1,11 @@
 #Test states.py
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import numpy as np
+from states import states
+
+import pytest
